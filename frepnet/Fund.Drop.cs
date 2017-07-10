97a78090c1bd6ee9b666d461b68abc10f808c7cf
polonair <fixnim@gmail.com>
2017-07-10 23:49:16 +0400

    Release 1.0
    * initial release.

---
+++ frepnet/Fund.Drop.cs
@@ -0,0 +1,101 @@
+﻿using System;
+namespace frepnet
+{
+	partial class Fund
+	{
+		public string note1 { get { return this._Notes[0]; } }
+		public string note2 { get { return this._Notes[1]; } }
+		public string note3 { get { return this._Notes[2]; } }
+		public string note4 { get { return this._Notes[3]; } }
+		public string note5 { get { return this._Notes[4]; } }
+		public string note6 { get { return this._Notes[5]; } }
+		public string note7 { get { return this._Notes[6]; } }
+		public string fundName { get { return this._Name; } }
+		public double valueResearchRating { get { return this.ValueResearchRating; } }
+		public double totalBondSales { get { return this.TotalBondSales; } }
+		public int performanceScoreRank { get { return this.PerformanceScoreRank; } }
+		public double yesterdayNAV { get { return this.YesterdayNav; } }
+		public double todayNAV { get { return this.TodayNav; } }
+		public double changeInNAV { get { return this.ChangeInNAV; } }
+		public double percentageChangeInNAV { get { return this.PercentageChangeInNAV; } }
+		public int daysSinceLaunch { get { return this.DaysSinceLaunch; } }
+		public double performanceScore { get { return this.PerformanceScore; } }
+		public double performanceImprovementPercentage { get { return this.PerformanceImprovementPercentage; } }
+		public DateTime fundLaunchDate { get { return this._LaunchDate; } }
+		public int performanceImprovementPercentageRank { get { return this.PerformanceImprovementPercentageRank; } }
+		public int valueResearchRatingRank { get { return this.HighestRatingRank; } }
+		public double overallScore { get { return this.OverallScore; } }
+		public int highestRatingRank { get { return this.HighestRatingRank; } }
+		//--
+		public string Note1 { get { return this._Notes[0]; } }
+		public string Note2 { get { return this._Notes[1]; } }
+		public string Note3 { get { return this._Notes[2]; } }
+		public string Note4 { get { return this._Notes[3]; } }
+		public string Note5 { get { return this._Notes[4]; } }
+		public string Note6 { get { return this._Notes[5]; } }
+		public string Note7 { get { return this._Notes[6]; } }
+
+		public string FundID { get { return this._Id; } }
+		public string FundName { get { return this._Name; } }
+		public string FundLaunchDate { get { return string.Format("{0:dd MMMM yyyy}", this._LaunchDate); } }
+		public double previousNAV { get { return this.PreviousNav; } }
+		public double lowestNAV { get { return this.LowestNAV; } }
+		public double highestNAV { get { return this.HighestNAV; } }
+		public double todayRating { get { return this.ValueResearchRating; } }
+		public double todaySales { get { return this.TotalBondSales; } }
+		public double navChange { get { return this.ChangeInNAV; } }
+		public double navChangePercentage { get { return this.PercentageChangeInNAV; } }
+		public double navChangeLong { get { return this.ChangeInNavLong; } }
+		public double navChangeLongPercentage { get { return this.PercentageChangeInNAVLong; } }
+		public double overallScoreRank { get { return this.OverallScoreRank; } }
+
+		public string day1date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now); } }
+		public string day2date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(1))); } }
+		public string day3date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(2))); } }
+		public string day4date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(3))); } }
+		public string day5date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(4))); } }
+		public string day6date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(5))); } }
+		public string day7date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(6))); } }
+		public string day8date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(7))); } }
+		public string day9date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(8))); } }
+		public string day10date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(9))); } }
+		public string day11date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(10))); } }
+		public string day12date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(11))); } }
+		public string day13date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(12))); } }
+		public string day14date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(13))); } }
+		public string day15date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(14))); } }
+		public string day16date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(15))); } }
+		public string day17date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(16))); } }
+		public string day18date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(17))); } }
+		public string day19date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(18))); } }
+		public string day20date { get { return string.Format("{0:dd MMMM yyyy}", DateTime.Now.Subtract(TimeSpan.FromDays(19))); } }
+
+		private string __getDayNav(int shift)
+		{
+			if (this._History[shift] == null) return "None";
+			if (double.IsNaN(this._History[shift].Nav)) return "None";
+			return this._History[shift].Nav.ToString();
+		}
+
+		public string day1NAV { get { return this.__getDayNav(0); } }
+		public string day2NAV { get { return this.__getDayNav(1); } }
+		public string day3NAV { get { return this.__getDayNav(2); } }
+		public string day4NAV { get { return this.__getDayNav(3); } }
+		public string day5NAV { get { return this.__getDayNav(4); } }
+		public string day6NAV { get { return this.__getDayNav(5); } }
+		public string day7NAV { get { return this.__getDayNav(6); } }
+		public string day8NAV { get { return this.__getDayNav(7); } }
+		public string day9NAV { get { return this.__getDayNav(8); } }
+		public string day10NAV { get { return this.__getDayNav(9); } }
+		public string day11NAV { get { return this.__getDayNav(10); } }
+		public string day12NAV { get { return this.__getDayNav(11); } }
+		public string day13NAV { get { return this.__getDayNav(12); } }
+		public string day14NAV { get { return this.__getDayNav(13); } }
+		public string day15NAV { get { return this.__getDayNav(14); } }
+		public string day16NAV { get { return this.__getDayNav(15); } }
+		public string day17NAV { get { return this.__getDayNav(16); } }
+		public string day18NAV { get { return this.__getDayNav(17); } }
+		public string day19NAV { get { return this.__getDayNav(18); } }
+		public string day20NAV { get { return this.__getDayNav(19); } }
+	}
+}
---
+++ frepnet/Report.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace frepnet
+{
+	class Report
+	{
+		private string _Template = null;
+		private List<QueryResult> _Datas = new List<QueryResult>();
+		private DataBase _DataBase;
+
+		public string Template { get { return this._Template; } set { this._Template = value; } }
+
+		public Report(DataBase dataBase)
+		{
+			this._DataBase = dataBase;
+		}
+		public void Save(Settings settings)
+		{
+			if (this._Template != null)
+			{
+				DotLiquid.Template.NamingConvention = new DotLiquid.NamingConventions.CSharpNamingConvention();
+				DotLiquid.Template template = DotLiquid.Template.Parse(this._Template);
+
+				foreach (QueryResult data in this._Datas)
+				{
+					int length = data.Funds.Count;
+					if (length < 1) continue;
+					int pages = Math.Min((length / 50) + 1, 3);
+
+					for (int i = 0; i < pages; i++)
+					{
+						string fn = string.Format("Query_{0}_{1}_{2}_of_{3}.html",
+												  (int)data.Type, data.Category, i + 1, pages);
+						fn = this.EscapeFileName(fn);
+
+						Dictionary<string, object> env = new Dictionary<string, object>();
+
+						env.Add("fundCategory", data.Category);
+						env.Add("FundCategory", data.Category);
+						env.Add("mutualFunds", data.GetSlice(this._DataBase, i * 50, 50));
+						env.Add("currentPage", i + 1);
+						env.Add("currentPageNumber", i + 1);
+						env.Add("nextPage", ((i + 1) >= pages) ? (0) : (i + 2));
+						env.Add("nextPageNumber", ((i + 1) >= pages) ? (0) : (i + 2));
+						env.Add("totalPages", pages);
+						env.Add("date", DateTime.Now);
+						env.Add("datestamp", string.Format("{0:dd MMMM yyyy}", DateTime.Now));
+						env.Add("timestamp", string.Format("{0:hh.mm tt }", DateTime.Now));
+
+						string content = template.Render(DotLiquid.Hash.FromDictionary(env));
+
+						if (!Directory.Exists(settings.ExportDirectory)) Directory.CreateDirectory(settings.ExportDirectory);
+						File.WriteAllText(string.Format("{0}/{1}", settings.ExportDirectory, fn), content);
+						/*string dbg = "";
+						foreach (Fund f in data.GetSlice(this._DataBase, i * 50, 50)) dbg += f.PrintDebug();
+						File.WriteAllText(string.Format("{0}/{1}.txt", settings.ExportDirectory, fn), dbg);*/
+					}
+				}
+			}
+		}
+		string EscapeFileName(string fn)
+		{
+			string result = "";
+			foreach (char c in fn)
+			{
+				if (char.IsLetterOrDigit(c) || c == '_' || c == '.') result += c;
+				else result += '_';
+				while (result != result.Replace("__", "_")) result = result.Replace("__", "_");
+			}
+			return result;
+		}
+
+		internal void AddResult(QueryResult r) { this._Datas.Add(r); }
+	}
+}
